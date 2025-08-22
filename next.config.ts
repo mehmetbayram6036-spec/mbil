dff1403019335c8cb39f16c4083cc4f8b655aaa5
mehmetbayram6036-spec <mehmetbayram6036@gmail.com>
2025-08-22 14:01:07 +0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  turbopack: {
-    rules: {
-      '*.svg': {
-        loaders: ['@svgr/webpack'],
-        as: '*.js',
-      },
-    },
-  },
   images: {
-    domains: ['localhost'],
     unoptimized: true
   }
 };
